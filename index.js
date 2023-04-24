e702ee9b02e37b316740eb7e44490484cc3abd22
Jai Khurana <jaikhurana@pop-os.localdomain>
2023-04-24 15:06:29 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-//A express server, which will handle api 
+//A express server, which will handle api
 
-const express =  require('express');
-const bodyParser = require('body-parser');
-const cors =  require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/', (req,res) => {
-    res.send("hekko work");
+app.post("/", (req, res) => {
+  res.send({ message: "hekko work" });
 });
 
-app.listen(port,() => {
-    console.log('exp app');
+app.listen(port, () => {
+  console.log("exp app");
 });
--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-//create componenet
-
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
-function App(){
-  const [message, setMessage] = useState('');
-  const [response, setResponse] = useState('');
+function App() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    fetch('http://localhost:3001/',{
-      method: 'POST',
-      headers:{
-        'Content-Type':'application/json',
+    await fetch("http://localhost:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      bosy: JSON.stringify({message}),
+      bosy: JSON.stringify({ message }),
     })
       .then((res) => res.json())
       .then((data) => setResponse(data.message));
   };
 
-  return(
+  return (
     <div classname="App">
-      <from onSubmit={handleSubmit}>
+      <from>
         <textarea
           value={message}
-          onChange={(e) => setMessage(e.target.value)}>
-
-          </textarea>
-          <button type="submit"> Submit</button>
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="button" onClick={handleSubmit}>
+          {" "}
+          Submit
+        </button>
       </from>
       <div>{response}</div>
     </div>
   );
 }
 
-export default App
+export default App;
